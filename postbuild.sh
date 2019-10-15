a4688787f471ef8dcf861b81d7ddc89f24dc3b62
Dapo Afolabi <nms595@gmail.com>
2019-10-15 11:52:08 +0100

    Added new file and amended Postbuild.sh

--- postbuild.sh
+++ postbuild.sh
@@ -3,14 +3,11 @@
 sudo yum install epel-release -y
 sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 sudo yum update -y
+sudo yum install -y maven
 sudo yum  install -y python36u python36u-libs python36u-devel python36u -pip
 sudo pip install boto3
-sudo pip install -U pip
 sudo pip install -U virtualenv
 sudo yum -y install java-11-openjdk-devel
-sudo install maven -y
 sudo yum install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-
-
